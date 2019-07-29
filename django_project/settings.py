9b6071e407963cdb8d2049133626d69d17cde853
VickramMS <46955658+VickramMS@users.noreply.github.com>
2019-07-29 22:08:47 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import psycopg2
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,6 +86,8 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -142,3 +146,5 @@ CKEDITOR_CONFIGS={
         'width': "100%",
     },
 }
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
