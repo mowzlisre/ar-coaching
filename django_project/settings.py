5b658de438a0ff78463a857ee5dbabf0270a7ab4
Vishnu Varthan <varthanv26018@gmail.com>
2019-08-10 15:15:34 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,7 +130,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../blog/static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
