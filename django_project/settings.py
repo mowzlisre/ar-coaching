73be6144cfae146d495e3031366ccb811e85c1b4
Vishnu Varthan <varthanv26018@gmail.com>
2019-08-10 15:07:39 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't#11(^xnf%kxwe=zv%o+0q=%da=#akx+kb_@ev$4f!e84lj$l9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
