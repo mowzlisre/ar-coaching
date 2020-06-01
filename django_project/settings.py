5ff323d2ac8901aea4471821e93657df1b152086
VickramMS <mowlisri123@gmail.com>
2020-06-01 15:48:27 +0530

    Commmit

--- django_project/settings.py
+++ django_project/settings.py
@@ -76,6 +76,13 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+'''DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
 
 DATABASES = {
     'default': {
@@ -88,6 +95,7 @@ DATABASES = {
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
