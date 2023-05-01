80711e92100bf9a74333e17e564bfa8e6bccca7f
UniqFocuz <it@rebornx.io>
2023-05-01 14:57:10 +0530

    Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'postgres',
             'USER': 'postgres',
-            'PASSWORD': 'Mowli123',
-            'HOST': 'arcc.ctcchoa2jcad.ap-south-1.rds.amazonaws.com',
+            'PASSWORD': 'Arcc1234',
+            'HOST': 'database-1.cm64nzoyed5x.eu-west-2.rds.amazonaws.com',
             'PORT': '5432',
         }
     }
@@ -129,7 +129,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'https://arccphysics.s3.ap-southeast-2.amazonaws.com/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 
@@ -139,4 +139,3 @@ CKEDITOR_CONFIGS={
         'width': "100%",
     },
 }
-
