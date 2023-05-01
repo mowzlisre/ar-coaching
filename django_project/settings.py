0c6b077683e7102394cecf1f9c5eab88a748bb82
mowzlisre <46955658+mowzlisre@users.noreply.github.com>
2023-05-01 13:53:46 +0530

    Static

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,8 +128,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = 'https://arccphysics.s3.ap-southeast-2.amazonaws.com/static'
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = 'https://arccphysics.s3.ap-southeast-2.amazonaws.com/static'
 
 # Extra places for collectstatic to find static files.
 
